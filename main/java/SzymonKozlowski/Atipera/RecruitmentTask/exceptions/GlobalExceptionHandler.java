4ada4a959e70a62b9662436164bdeaa30d88c231
Szymon Kozlowski <szymonkozlowski1092@gmail.com>
2025-08-18 19:39:14 +0200

    Implement test for RecruitmentTaskController and refactor file names
    - Implement happy-path test for GithubController
    - Rename GithubService to RecruitmentTaskService
    - Rename GithubController to RecruitmentTaskController
    - Rename GithubClient to GithubService

--- main/java/SzymonKozlowski/Atipera/RecruitmentTask/exceptions/GlobalExceptionHandler.java
+++ main/java/SzymonKozlowski/Atipera/RecruitmentTask/exceptions/GlobalExceptionHandler.java
@@ -18,15 +18,6 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 
-    @ExceptionHandler(GithubApiException.class)
-    public ResponseEntity<ErrorResponse> handleGithubApiException(GithubApiException ex, HttpServletRequest request){
-        ErrorResponse error = new ErrorResponse(
-                HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                ex.getMessage());
-
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
-    }
-
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex, HttpServletRequest request){
         ErrorResponse error = new ErrorResponse(
